95aff8bf46d1ca7235f7ceda8d63530916593d83
aditi <aditigk9@gmail.com>
2023-05-15 20:38:04 +0530

    updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 function Navbar(){
     return(
         <div class="navigation-bar">
-            <nav class="navbar fixed-top navbar-expand-xl navbar-dark py-3 px-5" style={{backgroundColor: '#0E141B'}}>
+            <nav id="navigation" class="navbar fixed-top navbar-expand-xl navbar-dark py-3 px-5" style={{backgroundColor: '#0E141B'}}>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item pe-3">
                         <a class="nav-link" href="#home">Home</a>
